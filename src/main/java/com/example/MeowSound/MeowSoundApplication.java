a8051eefa0849d36cdde9ea7ea004638f7c139f0
Amin Ehterami <A.Ehterami@proton.me>
2024-06-03 03:39:05 +0330

    init

---
+++ src/main/java/com/example/MeowSound/MeowSoundApplication.java
@@ -0,0 +1,13 @@
+package com.example.MeowSound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeowSoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeowSoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MeowSound/MeowSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MeowSound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeowSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
